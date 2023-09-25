413dc4d1b0a744579a88ebcd8d72fe18fa7bf256
puly garcia <pulygarcia09@gmail.com>
2023-09-25 17:07:12 -0300

    changing to module sintax

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const express =  require('express');
+import express from 'express';
 
 //Set up the app
 const app = express();
@@ -6,7 +6,20 @@ const app = express();
 
 //define route
 app.get('/', (req, res) => {
-    res.send('Response')
+    const products = [
+        {
+            id: 1,
+            name: 'Producto 1',
+            price: 1212 
+        },
+        {
+            id: 2,
+            name: 'Producto 2',
+            price: 1212323 
+        }
+    ]
+
+    res.send(products)
 })
 
 
