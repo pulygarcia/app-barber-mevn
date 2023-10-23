8adccaecf0e593d07206668dc016a12959bc6c8d
puly garcia <pulygarcia09@gmail.com>
2023-10-23 17:13:02 -0300

    middleware finished, ready to continue with guard in front

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -125,7 +125,10 @@ const login = async (req, res) => {
 }
 
 const user = async (req, res) => {
-    console.log('Desde user');
+    //console.log(req.user);   //Getting req.user from middleware and returning it
+    res.json(
+        user
+    )
 }
 
 export{
